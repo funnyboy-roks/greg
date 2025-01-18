fb56c9aafbd7ac530e54d3896bf5f7c0a505ced6
funnyboy-roks <funnyboyroks@gmail.com>
2025-01-18 15:11:11 -0600

    better c

--- c/foo.c
+++ c/foo.c
@@ -1,79 +1,10 @@
-void _exit_(void) {
-    asm(
-        "li $v0, 10\n"
-        "syscall"
-    );
-}
-
-void print(char *str) {
-    asm(
-        "add $a0, %0\n"
-        "li $v0, 4\n"
-        "syscall"
-        :
-        : "r"(str)
-        : "a0", "$v0"
-    );
-}
-
-// void print(char *str) {
-//     asm(
-//         "add $a0, %0\n"
-//         "addi $v0, $zero, 4\n"
-//         "syscall"
-//         :
-//         : "r"(str)
-//         : "$v0"
-//     );
-// }
-
-typedef unsigned int size_t;
-
-void syscall0(size_t number) {
-    asm(
-        "move $v0, %0"
-        "syscall"
-        :
-        : "r"(n)
-        :
-    );
-}
-
-void syscall1(size_t number, size_t arg1) {
-    asm(
-        "move $a0, %0"
-        "move $v0, %1"
-        "syscall"
-        :
-        : "r"(n), "r"(arg1)
-        :
-    );
-}
-
-void print_int(unsigned int n) {
-    syscall1(1, (size_t)n);
-}
-
+#include "./syscall.h"
 
 void __start(void)
 {
-    unsigned int x = 35;
-    // int y = 34;
-    // int z = x + y;
-    // print_int(x);
-    // asm(
-    //     "move $4, %0\n"
-    //     "li $2, 1\n"
-    //     "syscall"
-    //     :
-    //     : "r"(x)
-    //     : "$2", "$4"
-    // );
-    // syscall1(1, x);
-    // syscall1(11, '\n');
-    print_int(x + 34);
+    for (int i = 0; i < 16; ++i) {
+        print_int(i);
+        syscall1(11, '\n');
+    }
     syscall0(10);
-    // 1 + 2;
-    // print("hello");
-    // exit();
 }
---
+++ c/syscall.h
@@ -0,0 +1,6 @@
+typedef unsigned int size_t;
+void _exit_(void);
+void print(char *str);
+void print_int(size_t n);
+void syscall0(size_t call);
+void syscall1(size_t call, size_t arg1);
